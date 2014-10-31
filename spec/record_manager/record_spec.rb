e8daad2f4fe6d4d282803e6ca16993161f526106
Justin Park <jpark3000@gmail.com>
2014-10-31 02:55:15 -0500

    specs output format of record object

--- spec/record_manager/record_spec.rb
+++ spec/record_manager/record_spec.rb
@@ -8,4 +8,8 @@ describe RecordManager::Record do
   it 'instantiates a record' do
     expect(record).to be_instance_of RecordManager::Record
   end
+
+  it 'outputs a record correctly' do
+    expect(record.to_output).to eql 'Park, Justin, Male, Blue, 12/11/1987'
+  end
 end
