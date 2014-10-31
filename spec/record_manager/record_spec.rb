70b3fa0d925f7bd12643f46ce8e22536fee51a7d
Justin Park <jpark3000@gmail.com>
2014-10-31 02:52:29 -0500

    Updates spec to use positional arguments
    This reflects a change made in the
    Record class to take positional
    arguments in order to accept a
    splatted array.

--- spec/record_manager/record_spec.rb
+++ spec/record_manager/record_spec.rb
@@ -3,12 +3,9 @@
 require 'record_manager/record'
 
 describe RecordManager::Record do
-    let(:record) { RecordManager::Record.new({ first_name: 'Justin', 
-                                               last_name: 'Park', 
-                                               gender: 'Male', 
-                                               favorite_color: 'Blue', 
-                                               dob: '12-11-1987' }) } 
+  let(:record) { RecordManager::Record.new('Park', 'Justin', 'Male', 'Blue', '12-11-1987') } 
+  
   it 'instantiates a record' do
-    expect(record).to be_instance_of(RecordManager::Record)
+    expect(record).to be_instance_of RecordManager::Record
   end
 end
