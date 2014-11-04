3beda2e45bba2b02d894ac9afe38d5f9f3379355
Justin Park <jpark3000@gmail.com>
2014-11-03 18:28:11 -0600

    adds encoding utf-8 line

--- lib/record_manager.rb
+++ lib/record_manager.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'record_manager/parser'
 require 'record_manager/record'
 require 'record_manager/records'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'record_manager'
 
 def test_records
