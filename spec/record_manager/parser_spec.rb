07f1d52f37b995a4c8409b6e331c2a482e784ce0
Justin Park <jpark3000@gmail.com>
2014-11-03 18:03:17 -0600

    adds specs for parsing a single line
    also adds context for parsing files spec and updates
    naming

--- spec/record_manager/parser_spec.rb
+++ spec/record_manager/parser_spec.rb
@@ -6,23 +6,42 @@ describe RecordManager::Parser do
   let(:parser) { RecordManager::Parser.new }
   let(:record_array) { Array.new(5, RecordManager::Record) }
 
-  it 'loads a pipe delimited text file into an array of record objects' do
-    records = parser.load_records('pipes.txt')
-    expect(records.map(&:class)).to match_array record_array
+  context 'when parsing a file' do
+    it 'loads a pipe delimited text file into an array of record objects' do
+      records = parser.load_file('pipes.txt')
+      expect(records.map(&:class)).to match_array record_array
+    end
+
+    it 'loads a space delimited text file into an array of record objects' do
+      records = parser.load_file('spaces.txt')
+      expect(records.map(&:class)).to match_array record_array
+    end
+
+    it 'loads a text delimited text file into an array of record objects' do
+      records = parser.load_file('commas.txt')
+      expect(records.map(&:class)).to match_array record_array
+    end
   end
 
-  it 'loads a space delimited text file into an array of record objects' do
-    records = parser.load_records('spaces.txt')
-    expect(records.map(&:class)).to match_array record_array
-  end
+  context 'when parsing a single data line' do
+    it 'handles a pipe delimited string' do
+      expect(parser.load_data_line('Park | Justin | Male | Blue | 12-11-1987'))
+        .to be_instance_of RecordManager::Record
+    end
 
-  it 'loads a text delimited text file into an array of record objects' do
-    records = parser.load_records('commas.txt')
-    expect(records.map(&:class)).to match_array record_array
+    it 'handles a comma delimited string' do
+      expect(parser.load_data_line('Park, Justin, Male, Blue, 12-11-1987'))
+        .to be_instance_of RecordManager::Record
+    end
+
+    it 'handles a space delimted string' do
+      expect(parser.load_data_line('Park, Justin, Male, Blue, 12-11-1987'))
+        .to be_instance_of RecordManager::Record
+    end
   end
 
   it 'parses a record correctly' do
-    record = parser.load_records('pipes.txt').first
-    expect(record.format).to eql "Jarmusch, Jim, Male, Black, 1/22/1953"
+    record = parser.load_file('pipes.txt').first
+    expect(record.to_output).to eql "Jarmusch, Jim, Male, Black, 1/22/1953"
   end
 end
